d2a0c27d3b1f8edbf1b648e4e0b09becb01e5e34
Krish Gupta <krishrajgupta129@gmail.com>
2026-04-18 22:39:45 +0530

    gradecalculator

---
+++ gradecalculator.py
@@ -0,0 +1,18 @@
+grade = int(input("enter your grade:-"))
+if(grade>=90):
+    print("what no puhh does to a man😭✌️")
+elif(grade>=80 and grade<=90):
+  print("dnmm thats good😏")
+elif(grade<=80 and grade>=70):
+  print("its cool its cool😁")
+elif(grade<=70 and grade>=60):
+  print("thats fine🥲")
+else:
+    print("nahh nigga you done for💀😶‍🌫️")
+ 
+
+
+
+
+
+
