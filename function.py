e48da3780fdf4920a2ef6e8e99002681b016d7ef
krish Gupta <krishrajgupta129@gmail.com>
2026-08-06 15:48:07 +0530

    function

---
+++ function.py
@@ -0,0 +1,5 @@
+#text = "  hello world  "
+#text.strip() # removes extra spaces
+text = "  hello world  "
+text.count("o") # counts the number of occurrences of "o" in the string
+text = " python programming "
