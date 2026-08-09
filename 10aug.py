ff636dbb05499271d35bf791a3b3ed5fee3df677
krish Gupta <krishrajgupta129@gmail.com>
2026-08-09 09:41:01 +0530

    remove element

---
+++ 10aug.py
@@ -0,0 +1,6 @@
+
+    #dict1={"name":"Satyam Raj Singh", "age":25, "grade":"A"
+
+print(f"Your name is : {dict1['name']}")
+print(f"Your age is : {dict1['age']}")
+print(f"Your grade is : {dict1['grade']}")
--- removingbyelements.py
+++ removingbyelements.py
@@ -1,2 +1,4 @@
 top_movies = ["The Shawshank Redemption", "The Godfather", "The Dark Knight", "Pulp Fiction", "Forrest Gump"]
 del top_movies[2]  # Removes "The Dark Knight" from the list
+for movie in top_movies:
+    print(movie)
