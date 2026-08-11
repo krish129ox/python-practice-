0a9b43dce9111b9d7b93b3b444fb2cf30943ee3c
krish Gupta <krishrajgupta129@gmail.com>
2026-08-11 16:29:26 +0530

    11aug

---
+++ 11aug.py
@@ -0,0 +1,13 @@
+dict={'apple':20,'banana':30,'orange':40}
+#print(list(dict.keys()))
+#print(list(dict.values()))
+print('apple' in dict)
+s={1,2,3,4,5}
+s.add(6)
+print(s)
+set1={1,2,3,4,5}
+set2={4,5,6,7,8}
+union_set_method=set1.union(set2)
+print(union_set_method)
+print(s1 | s2)
+    
