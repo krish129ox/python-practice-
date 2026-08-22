214da8a6839842edc98142208339b8a78d781da3
krish Gupta <krishrajgupta129@gmail.com>
2026-08-22 19:52:31 +0530

    22 aug

---
+++ Simple_timer_decorator.py
@@ -0,0 +1,9 @@
+import time, functools
+def timer(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        print(f"{func.__name__} took {time.time()-start:.4f}s")
+        return result
+    return wrapper
