955e33fb5cbb81be06c651fb56ea764eaf208289
krish Gupta <krishrajgupta129@gmail.com>
2026-08-25 20:12:42 +0530

    25 aug

---
+++ Merge_two_sorted_lists.py
@@ -0,0 +1,9 @@
+def merge_sorted(a, b):
+    result = []
+    i = j = 0
+    while i < len(a) and j < len(b):
+        if a[i] <= b[j]:
+            result.append(a[i]); i += 1
+        else:
+            result.append(b[j]); j += 1
+    return result + a[i:] + b[j:]
