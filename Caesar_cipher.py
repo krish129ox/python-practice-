e42d88257c4a0fb63a8c1d41683a4f7ef3c4ef44
krish Gupta <krishrajgupta129@gmail.com>
2026-09-02 22:06:34 +0530

    2 sep

---
+++ Caesar_cipher.py
@@ -0,0 +1,9 @@
+def caesar_encrypt(text, shift):
+    result = ""
+    for c in text:
+        if c.isalpha():
+            base = ord('A') if c.isupper() else ord('a')
+            result += chr((ord(c) - base + shift) % 26 + base)
+        else:
+            result += c
+    return result
