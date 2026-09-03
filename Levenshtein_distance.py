a9b4f395ace8b9a0b7f540b8e0515f486d4eba7b
krish Gupta <krishrajgupta129@gmail.com>
2026-09-03 20:50:51 +0530

    3 sep

---
+++ Levenshtein_distance.py
@@ -0,0 +1,12 @@
+def levenshtein(a, b):
+    if len(a) < len(b):
+        return levenshtein(b, a)
+    if len(b) == 0:
+        return len(a)
+    prev = list(range(len(b)+1))
+    for i, ca in enumerate(a):
+        curr = [i+1]
+        for j, cb in enumerate(b):
+            curr.append(min(prev[j+1]+1, curr[j]+1, prev[j]+(ca != cb)))
+        prev = curr
+    return prev[-1]
