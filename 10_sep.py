aa6ebf3a680260424e3b868bd92d336cd0c43494
krish Gupta <krishrajgupta129@gmail.com>
2026-09-10 16:14:20 +0530

    10 sep

---
+++ 10_sep.py
@@ -0,0 +1,8 @@
+class EventEmitter:
+    def __init__(self):
+        self.listeners = {}
+    def on(self, event, callback):
+        self.listeners.setdefault(event, []).append(callback)
+    def emit(self, event, *args, **kwargs):
+        for cb in self.listeners.get(event, []):
+            cb(*args, **kwargs)
